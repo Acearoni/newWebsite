ae2b2d7e2d0d171929ee663e264ab8ac3593fb36
Acearoni <awoodson.contact@gmail.com>
2025-05-27 22:22:10 -0500

    Messing with styling, added borders to see my building blocks. Will work on hovering styling and *active* tabs next.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,11 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav-bar">
-                <div className="nav-link"><Link to="/local">Local</Link></div>
-                <div className="nav-link"><Link to="/personal">Personal</Link></div>
+                <div className="nav-link"><Link to="/local">LOCAL</Link></div>
+                <div className="nav-link"><Link to="/personal">PERSONAL</Link></div>
                 <h1 className="title">My Blog</h1>
-                <div className="nav-link"><Link to="/gaming">Gaming</Link></div>
-                <div className="nav-link"><Link to="/socials">Socials</Link></div>
+                <div className="nav-link"><Link to="/gaming">GAMING</Link></div>
+                <div className="nav-link"><Link to="/socials">SOCIALS</Link></div>
             </nav>
         </header>
     );
